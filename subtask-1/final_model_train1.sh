f6b8202f5d9de22cf3abb406a0ac17853360852c
Richard Jonker <richard.jonker@ua.pt>
2023-10-04 09:52:14 +0000

    run training

---
+++ subtask-1/final_model_train1.sh
@@ -0,0 +1,16 @@
+CUDA_VISIBLE_DEVICES=1 python hf_trainer_full.py lcampillos/roberta-es-clinical-trials-ner --model_type_arch bilstm --context 4 --augmentation ukn --aug_prob 0.5 --percentage_tags 0.2
+CUDA_VISIBLE_DEVICES=1 python hf_trainer_full.py lcampillos/roberta-es-clinical-trials-ner --model_type_arch bilstm --context 4 --augmentation ukn --aug_prob 0.5 --percentage_tags 0.4
+CUDA_VISIBLE_DEVICES=1 python hf_trainer_full.py lcampillos/roberta-es-clinical-trials-ner --model_type_arch bilstm --context 4 --augmentation ukn --aug_prob 0.5 --percentage_tags 0.5
+CUDA_VISIBLE_DEVICES=1 python hf_trainer_full.py lcampillos/roberta-es-clinical-trials-ner --model_type_arch bilstm --context 4 --augmentation ukn --aug_prob 0.5 --percentage_tags 0.6
+CUDA_VISIBLE_DEVICES=1 python hf_trainer_full.py lcampillos/roberta-es-clinical-trials-ner --model_type_arch bilstm --context 4 --augmentation ukn --aug_prob 0.5 --percentage_tags 0.8
+CUDA_VISIBLE_DEVICES=1 python hf_trainer_full.py lcampillos/roberta-es-clinical-trials-ner --model_type_arch bilstm --context 4 --augmentation ukn --aug_prob 0.5 --percentage_tags 1
+
+CUDA_VISIBLE_DEVICES=1 python hf_trainer_full.py lcampillos/roberta-es-clinical-trials-ner --model_type_arch bilstm --context 4 --augmentation ukn --aug_prob 0.7 --percentage_tags 0.2
+CUDA_VISIBLE_DEVICES=1 python hf_trainer_full.py lcampillos/roberta-es-clinical-trials-ner --model_type_arch bilstm --context 4 --augmentation ukn --aug_prob 0.7 --percentage_tags 0.4
+CUDA_VISIBLE_DEVICES=1 python hf_trainer_full.py lcampillos/roberta-es-clinical-trials-ner --model_type_arch bilstm --context 4 --augmentation ukn --aug_prob 0.7 --percentage_tags 0.5
+CUDA_VISIBLE_DEVICES=1 python hf_trainer_full.py lcampillos/roberta-es-clinical-trials-ner --model_type_arch bilstm --context 4 --augmentation ukn --aug_prob 0.7 --percentage_tags 0.6
+CUDA_VISIBLE_DEVICES=1 python hf_trainer_full.py lcampillos/roberta-es-clinical-trials-ner --model_type_arch bilstm --context 4 --augmentation ukn --aug_prob 0.7 --percentage_tags 0.8
+CUDA_VISIBLE_DEVICES=1 python hf_trainer_full.py lcampillos/roberta-es-clinical-trials-ner --model_type_arch bilstm --context 4 --augmentation ukn --aug_prob 0.7 --percentage_tags 1
+
+CUDA_VISIBLE_DEVICES=1 python hf_trainer_full.py lcampillos/roberta-es-clinical-trials-ner --model_type_arch bilstm --context 4 --augmentation random --aug_prob 0.7 --percentage_tags 0.8
+CUDA_VISIBLE_DEVICES=1 python hf_trainer_full.py lcampillos/roberta-es-clinical-trials-ner --model_type_arch bilstm --context 4 --augmentation random --aug_prob 0.7 --percentage_tags 1
---
+++ subtask-1/final_model_train2.sh
@@ -0,0 +1,16 @@
+python hf_trainer_full.py lcampillos/roberta-es-clinical-trials-ner --model_type_arch bilstm --context 4 --augmentation random --aug_prob 0.5 --percentage_tags 0.2
+python hf_trainer_full.py lcampillos/roberta-es-clinical-trials-ner --model_type_arch bilstm --context 4 --augmentation random --aug_prob 0.5 --percentage_tags 0.4
+python hf_trainer_full.py lcampillos/roberta-es-clinical-trials-ner --model_type_arch bilstm --context 4 --augmentation random --aug_prob 0.5 --percentage_tags 0.5
+python hf_trainer_full.py lcampillos/roberta-es-clinical-trials-ner --model_type_arch bilstm --context 4 --augmentation random --aug_prob 0.5 --percentage_tags 0.6
+python hf_trainer_full.py lcampillos/roberta-es-clinical-trials-ner --model_type_arch bilstm --context 4 --augmentation random --aug_prob 0.5 --percentage_tags 0.8
+python hf_trainer_full.py lcampillos/roberta-es-clinical-trials-ner --model_type_arch bilstm --context 4 --augmentation random --aug_prob 0.5 --percentage_tags 1
+
+python hf_trainer_full.py lcampillos/roberta-es-clinical-trials-ner --model_type_arch bilstm --context 4 --augmentation ukn --aug_prob 0.8 --percentage_tags 0.2
+python hf_trainer_full.py lcampillos/roberta-es-clinical-trials-ner --model_type_arch bilstm --context 4 --augmentation ukn --aug_prob 0.8 --percentage_tags 0.4
+python hf_trainer_full.py lcampillos/roberta-es-clinical-trials-ner --model_type_arch bilstm --context 4 --augmentation ukn --aug_prob 0.8 --percentage_tags 0.5
+python hf_trainer_full.py lcampillos/roberta-es-clinical-trials-ner --model_type_arch bilstm --context 4 --augmentation ukn --aug_prob 0.8 --percentage_tags 0.6
+python hf_trainer_full.py lcampillos/roberta-es-clinical-trials-ner --model_type_arch bilstm --context 4 --augmentation ukn --aug_prob 0.8 --percentage_tags 0.8
+python hf_trainer_full.py lcampillos/roberta-es-clinical-trials-ner --model_type_arch bilstm --context 4 --augmentation ukn --aug_prob 0.8 --percentage_tags 1
+
+python hf_trainer_full.py lcampillos/roberta-es-clinical-trials-ner --model_type_arch bilstm --context 4 --augmentation random --aug_prob 0.8 --percentage_tags 0.8
+python hf_trainer_full.py lcampillos/roberta-es-clinical-trials-ner --model_type_arch bilstm --context 4 --augmentation random --aug_prob 0.8 --percentage_tags 1
